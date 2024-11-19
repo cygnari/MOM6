d27b9b38fa1e990bd6040621fc50c64bbd423450
Anthony Chen <cygnari@berkeley.edu>
2024-11-19 16:36:47 -0500

    Stuff

--- src/core/MOM_dynamics_split_RK2.F90
+++ src/core/MOM_dynamics_split_RK2.F90
@@ -1477,9 +1477,11 @@ subroutine initialize_dyn_split_RK2(u, v, h, tv, uh, vh, eta, Time, G, GV, US, p
   call CoriolisAdv_init(Time, G, GV, US, param_file, diag, CS%ADp, CS%CoriolisAdv)
   ! if (CS%calculate_SAL) call SAL_init(G, US, param_file, CS%SAL_CSp) ! sph harm SAL
   if (CS%calculate_SAL) call sal_conv_init(CS%SAL_convCSp, G) ! convolution SAL
+  print *, 'sal conv init done'
   if (CS%use_tides) call tidal_forcing_init(Time, G, US, param_file, CS%tides_CSp)
   call PressureForce_init(Time, G, GV, US, param_file, diag, CS%PressureForce_CSp, &
                           CS%SAL_CSp, CS%tides_CSp, CS%SAL_convCSp)
+  print *, 'pressure init done'
   call hor_visc_init(Time, G, GV, US, param_file, diag, CS%hor_visc, ADp=CS%ADp)
   call vertvisc_init(MIS, Time, G, GV, US, param_file, diag, CS%ADp, dirs, ntrunc, CS%vertvisc_CSp)
   CS%set_visc_CSp => set_visc
