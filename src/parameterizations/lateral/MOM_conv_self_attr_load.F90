ec3c209c853f8e0f5123b61c0a70bbcf3bf6aed6
Anthony Chen <cygnari@berkeley.edu>
2024-11-20 16:15:20 -0500

    stuff

--- src/parameterizations/lateral/MOM_conv_self_attr_load.F90
+++ src/parameterizations/lateral/MOM_conv_self_attr_load.F90
@@ -641,21 +641,21 @@ subroutine calculate_communications(sal_ct, xg, yg, zg, G)
         print *, 'id ', id, ' receive ', points_needed_from_proc(i), ' from ', i-1
         print *, 'id ', id, ' give ', points_to_give_proc(i), ' to ', i-1
     enddo
-    do i=0, p-1 ! send needed point counts
-        if (i .ne. id) then
-            pelist(1) = min(i, id)
-            pelist(2) = max(i, id)
-            call broadcast(points_needed_from_proc(i+1), id, pelist)
-        endif
-    enddo
+    ! do i=0, p-1 ! send needed point counts
+    !     if (i .ne. id) then
+    !         pelist(1) = min(i, id)
+    !         pelist(2) = max(i, id)
+    !         call broadcast(points_needed_from_proc(i+1), id, pelist)
+    !     endif
+    ! enddo
 
-    do i=0, p-1 ! receive point counts to give
-        if (i .ne. id) then
-            pelist(1) = min(i, id)
-            pelist(2) = max(i, id)
-            call broadcast(points_to_give_proc(i+1), i, pelist)
-        endif
-    enddo
+    ! do i=0, p-1 ! receive point counts to give
+    !     if (i .ne. id) then
+    !         pelist(1) = min(i, id)
+    !         pelist(2) = max(i, id)
+    !         call broadcast(points_to_give_proc(i+1), i, pelist)
+    !     endif
+    ! enddo
 
     call sync_PEs()
 
@@ -677,34 +677,34 @@ subroutine calculate_communications(sal_ct, xg, yg, zg, G)
 
     print *, id, 'here 4 1'
 
-    do i=0, p-1 ! send point indices
-        ! pelist(2) = i
-        if (i.ne.id) then
-            if (points_needed_from_proc(i+1)>0) then
-                ! print *, 'id i needed', id, i, points_needed_from_proc(i+1)
-                print *, 'id ', id, ' receive ', points_needed_from_proc(i+1), ' from ', i
-                pelist(1) = min(i, id)
-                pelist(2) = max(i, id)
-                call broadcast(points_from_proc_i(:,i+1), points_needed_from_proc(i+1), id, pelist)
-                call broadcast(points_from_proc_j(:,i+1), points_needed_from_proc(i+1), id, pelist)
-            endif
-        endif
-    enddo
+    ! do i=0, p-1 ! send point indices
+    !     ! pelist(2) = i
+    !     if (i.ne.id) then
+    !         if (points_needed_from_proc(i+1)>0) then
+    !             ! print *, 'id i needed', id, i, points_needed_from_proc(i+1)
+    !             print *, 'id ', id, ' receive ', points_needed_from_proc(i+1), ' from ', i
+    !             pelist(1) = min(i, id)
+    !             pelist(2) = max(i, id)
+    !             call broadcast(points_from_proc_i(:,i+1), points_needed_from_proc(i+1), id, pelist)
+    !             call broadcast(points_from_proc_j(:,i+1), points_needed_from_proc(i+1), id, pelist)
+    !         endif
+    !     endif
+    ! enddo
 
     print *, id, 'here 4 2'
 
-    do i=0, p-1 ! receive point indices
-        ! pelist(2) = i
-        if (i.ne.id) then
-            if (points_to_give_proc(i+1)>0) then
-                print *, 'id ', id, ' give ', points_to_give_proc(i+1), ' to ', i
-                pelist(1) = min(i, id)
-                pelist(2) = max(i, id)
-                call broadcast(points_to_give_proc_i(:,i+1), points_to_give_proc(i+1), i, pelist)
-                call broadcast(points_to_give_proc_j(:,i+1), points_to_give_proc(i+1), i, pelist)
-            endif
-        endif
-    enddo
+    ! do i=0, p-1 ! receive point indices
+    !     ! pelist(2) = i
+    !     if (i.ne.id) then
+    !         if (points_to_give_proc(i+1)>0) then
+    !             print *, 'id ', id, ' give ', points_to_give_proc(i+1), ' to ', i
+    !             pelist(1) = min(i, id)
+    !             pelist(2) = max(i, id)
+    !             call broadcast(points_to_give_proc_i(:,i+1), points_to_give_proc(i+1), i, pelist)
+    !             call broadcast(points_to_give_proc_j(:,i+1), points_to_give_proc(i+1), i, pelist)
+    !         endif
+    !     endif
+    ! enddo
 
     print *, id, 'here 4 3'
 
