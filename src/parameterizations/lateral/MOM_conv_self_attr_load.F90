8b766e8f6d6bec7ec7804f2f1607897af69d2b27
Anthony Chen <cygnari@berkeley.edu>
2025-02-14 13:23:20 -0500

    Changes

--- src/parameterizations/lateral/MOM_conv_self_attr_load.F90
+++ src/parameterizations/lateral/MOM_conv_self_attr_load.F90
@@ -1039,6 +1039,8 @@ subroutine sal_conv_init(sal_ct, G, param_file)
         call sum_across_PEs(yg1d, pointcount)
         call sum_across_PEs(zg1d, pointcount)
 
+        print *, "SAL Conv init: tree traversal"
+
         ! xg/yg/zg is now a copy of all the points from all the processors
         call tree_traversal(G, sal_ct%tree_struct, xg1d, yg1d, zg1d, sal_ct%cluster_thresh, pointcount) ! constructs cubed sphere tree
         max_level = sal_ct%tree_struct(size(sal_ct%tree_struct))%level
@@ -1058,6 +1060,7 @@ subroutine sal_conv_init(sal_ct, G, param_file)
         call assign_points_to_panels(G, sal_ct%tree_struct, xc1d, yc1d, zc1d, sal_ct%points_panels, &
                                         max_level, sal_ct%point_leaf_panel) 
 
+        print *, "SAL Conv init: interaction list computation"
         ! compute the interaction lists for the target points in the target domain
         if (sal_ct%use_fmm) then
             call interaction_list_compute_fmm(sal_ct%pp_interactions, sal_ct%pc_interactions, sal_ct%cp_interactions, &
@@ -1069,6 +1072,7 @@ subroutine sal_conv_init(sal_ct, G, param_file)
         endif
         call sync_PEs()
 
+        print *, "SAL Conv init: communication calculations"
         ! compute communication patterns 
         call calculate_communications(sal_ct, xg1d, yg1d, zg1d, G)
 
